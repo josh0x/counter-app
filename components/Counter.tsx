d3781b8dcdeb6cb06f3d28366bd3cfe3c7923884
Josh <joshuabowers.dev@gmail.com>
2022-10-19 21:17:18 +0200

    big buttons...

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import styles from "../styles/Home.module.css";
 
 function Count() {
   let countNum = 0;
   return (
     <div>
-      <h1>{countNum}</h1>
-      <button>+</button>
-      <button>-</button>
-      <button>Reset</button>
+      <h1 className={styles.title}>Counter app ++</h1>
+      <div className={styles.title}>
+        <h1 >{countNum}</h1>
+        <button className="m-10 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          <span>+</span>
+        </button>
+        <button className="m-10 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          <span>-</span>
+        </button>
+        <button className="m-10 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          <span>Reset</span>
+        </button>
+      </div>
     </div>
   );
 }
