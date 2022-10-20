e9c702c5813feb99aab1155ff65d203c72f5eeb0
Josh <joshuabowers.dev@gmail.com>
2022-10-20 14:17:18 +0200

    styles

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -4,20 +4,19 @@ import styles from "../styles/Home.module.css";
 function Count() {
   let countNum = 0;
   return (
-    <div>
-      <h1 className={styles.title}>Count!</h1>
+    <div className="grid h-screen place-items-center">
       <div className={styles.title}>
-        <h1 >{countNum}</h1>
+        <h1 className={styles.title}>{countNum}</h1>
         <div className=" border-t-4 border-black pt-10">
-        <button className="btn mx-8">
-          <span className="btn__text px-4">+</span>
-        </button>
-        <button className="btn mx-8">
-          <span className="btn__text">-</span>
-        </button>
-        <button className="btn mx-8">
-          <span className="btn__text">Reset</span>
-        </button>
+          <button className="btn mx-8">
+            <span className="btn__text px-4">+</span>
+          </button>
+          <button className="btn mx-8">
+            <span className="btn__text">-</span>
+          </button>
+          <button className="btn mx-8">
+            <span className="btn__text">Reset</span>
+          </button>
         </div>
       </div>
     </div>
